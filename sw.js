9cc238fe3d88cdca82b0e8cdde098e0a8888d9dc
saddamalkadi <tntntt830@gmail.com>
2026-03-03 06:37:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,36 +2,19 @@ const CACHE_NAME = 'book-summarizer-cache-v1';
 const ASSETS = [
   './',
   './index.html',
-  './index_ocr.html',  // دعم للصفحة التي تحوي OCR
+  './index_ocr.html',
   './manifest.webmanifest',
-  './app_ocr.js'       // ملف الجافاسكريبت الجديد
+  './app_ocr.js'
 ];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
+  event.waitUntil(caches.keys().then((keys) =>
+    Promise.all(keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : undefined)))
+  ));
 });
-
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
